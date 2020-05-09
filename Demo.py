5cac871d6d9947e9510d78451d40f33a7d48e234
VanHung <hung125.hust.@gmail.com>
2020-05-09 11:17:47 +0700

    first commit

---
+++ Demo.py
@@ -0,0 +1,10 @@
+from record_module import *
+from username import *
+
+
+print("Press enter to record")
+record_audio()
+print("done!!!")
+
+print("recognizing.........")
+print("Hello: " + test1())
---
+++ train.py
@@ -0,0 +1,13 @@
+from trainrecord import *
+from GMM1 import *
+print("Please enter your name: ")
+name = input();
+
+
+recordTrain(name);
+
+print("Training........")
+traine(name)
+print("Train done!!!")
+
+
